dc4e4bf346876b0f8c53f90df8b612c24e83b80f
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-09-14 19:27:14 -0400

    Add V2 native script-thread runtime interface

--- src/game/GameRuntime.hpp
+++ src/game/GameRuntime.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include "types/ScriptTypes.hpp"
+
 #include <atomic>
+#include <cstdint>
+#include <deque>
+#include <functional>
+#include <mutex>
 
 namespace TutonesV2::Game
 {
@@ -12,8 +18,26 @@ namespace TutonesV2::Game
         bool Initialize() noexcept;
         void Shutdown() noexcept;
         [[nodiscard]] bool IsInitialized() const noexcept;
+        [[nodiscard]] bool NativeReady() const noexcept;
+        [[nodiscard]] bool NativeCanaryPassed() const noexcept;
+
+        void OnScriptSchedulerTick() noexcept;
+        bool Enqueue(std::function<void()> task);
 
     private:
+        Types::ScriptThread* FindExecutionThread() const noexcept;
+        void DrainTasks() noexcept;
+
         std::atomic_bool m_Initialized{};
+        std::atomic_bool m_ShuttingDown{};
+        std::atomic_bool m_NativeCanaryPassed{};
+        std::atomic_uint32_t m_ActiveCallbacks{};
+        bool m_NativeInitAttempted{};
+        bool m_NativeCanaryFailureLogged{};
+        bool m_LoggedNoScriptThread{};
+        bool m_LoggedNoTls{};
+
+        std::mutex m_TaskMutex;
+        std::deque<std::function<void()>> m_Tasks;
     };
 }
