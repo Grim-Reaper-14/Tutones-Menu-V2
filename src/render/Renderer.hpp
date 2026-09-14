b040fa10c90a97634fdc855efa86289c8b57a416
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-09-14 16:59:20 -0400

    Define DX12 renderer state

--- src/render/Renderer.hpp
+++ src/render/Renderer.hpp
@@ -1,6 +1,14 @@
 #pragma once
 
+#include <Windows.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <wrl/client.h>
+
 #include <atomic>
+#include <cstdint>
+#include <mutex>
+#include <vector>
 
 namespace TutonesV2::Render
 {
@@ -13,7 +21,42 @@ namespace TutonesV2::Render
         void Shutdown() noexcept;
         [[nodiscard]] bool IsInitialized() const noexcept;
 
+        void CaptureCommandQueue(ID3D12CommandQueue* commandQueue) noexcept;
+        void OnPresent(IDXGISwapChain* swapChain) noexcept;
+        void OnBeforeResize(IDXGISwapChain* swapChain) noexcept;
+        LRESULT HandleWindowMessage(HWND window, UINT message, WPARAM wParam, LPARAM lParam) noexcept;
+
     private:
+        struct FrameContext final
+        {
+            Microsoft::WRL::ComPtr<ID3D12CommandAllocator> CommandAllocator;
+            Microsoft::WRL::ComPtr<ID3D12Resource> BackBuffer;
+            D3D12_CPU_DESCRIPTOR_HANDLE Rtv{};
+            std::uint64_t FenceValue{};
+        };
+
+        bool InitializeSwapChain(IDXGISwapChain* swapChain) noexcept;
+        void RenderMenuFrame() noexcept;
+        void ResetSwapChainState(bool restoreWindowProc) noexcept;
+
         std::atomic_bool m_Initialized{};
+        std::atomic_bool m_RenderReady{};
+        std::atomic<ID3D12CommandQueue*> m_CommandQueue{};
+        std::mutex m_StateMutex;
+
+        Microsoft::WRL::ComPtr<IDXGISwapChain3> m_SwapChain;
+        Microsoft::WRL::ComPtr<ID3D12Device> m_Device;
+        Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_RtvHeap;
+        Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_SrvHeap;
+        Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_CommandList;
+        Microsoft::WRL::ComPtr<ID3D12Fence> m_Fence;
+        std::vector<FrameContext> m_Frames;
+
+        HWND m_Window{};
+        WNDPROC m_OriginalWindowProc{};
+        DXGI_FORMAT m_BackBufferFormat{DXGI_FORMAT_UNKNOWN};
+        UINT m_RtvDescriptorSize{};
+        std::uint64_t m_NextFenceValue{};
+        bool m_ImGuiReady{};
     };
 }
