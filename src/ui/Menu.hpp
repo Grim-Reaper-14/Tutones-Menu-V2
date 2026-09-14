58a27115caeef0d4429cf9c203e20bf8d1262771
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-09-14 17:00:20 -0400

    Add F5 menu state

--- src/ui/Menu.hpp
+++ src/ui/Menu.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
+#include <atomic>
+
 namespace TutonesV2::UI
 {
     class Menu final
     {
     public:
         static Menu& Get() noexcept;
+
+        void Toggle() noexcept;
+        void SetOpen(bool open) noexcept;
+        [[nodiscard]] bool IsOpen() const noexcept;
         void Render() noexcept;
+
+    private:
+        std::atomic_bool m_Open{};
     };
 }
