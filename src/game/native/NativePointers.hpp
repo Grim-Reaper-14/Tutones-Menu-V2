ac42ab2f6be9cbde5227ac822704177a9c09fa0d
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-09-23 10:42:55 -0400

    Expose script program and VM pointers

--- src/game/native/NativePointers.hpp
+++ src/game/native/NativePointers.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../memory/ModuleView.hpp"
+#include "../types/ScriptProgram.hpp"
 #include "../types/ScriptTypes.hpp"
 #include "NativeRegistry.hpp"
 
@@ -9,6 +10,11 @@
 namespace TutonesV2::Game::Native
 {
     using RunScriptThreadsFn = bool(*)(int operationsToExecute);
+    using ScriptVmFn = int(*)(
+        std::uint64_t* stack,
+        std::int64_t** globals,
+        Types::ScriptProgram* program,
+        void* context);
 
     class NativePointers final
     {
@@ -22,7 +28,9 @@ namespace TutonesV2::Game::Native
         [[nodiscard]] InitNativeTablesFn InitNativeTables() const noexcept;
         [[nodiscard]] RunScriptThreadsFn RunScriptThreads() const noexcept;
         [[nodiscard]] Types::AtArray<Types::ScriptThread*>* ScriptThreads() const noexcept;
+        [[nodiscard]] Types::ScriptProgram** ScriptPrograms() const noexcept;
         [[nodiscard]] std::int64_t** ScriptGlobals() const noexcept;
+        [[nodiscard]] ScriptVmFn ScriptVm() const noexcept;
         [[nodiscard]] bool* IsSessionStarted() const noexcept;
         [[nodiscard]] std::uint32_t* NetworkTime() const noexcept;
 
@@ -31,7 +39,9 @@ namespace TutonesV2::Game::Native
         InitNativeTablesFn m_InitNativeTables{};
         RunScriptThreadsFn m_RunScriptThreads{};
         Types::AtArray<Types::ScriptThread*>* m_ScriptThreads{};
+        Types::ScriptProgram** m_ScriptPrograms{};
         std::int64_t** m_ScriptGlobals{};
+        ScriptVmFn m_ScriptVm{};
         bool* m_IsSessionStarted{};
         std::uint32_t* m_NetworkTime{};
         std::atomic_bool m_Resolved{};
